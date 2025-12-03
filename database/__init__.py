c11267c9a8ee10c5f3470fb586c68d798b295bcd
Ansocialov1337 <napi2100mmr@gmail.com>
2025-12-03 22:23:56 +0300

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,30 +1,17 @@
-"""
-Модуль работы с базой данных
-"""
-from .models import User, GameHistory, ActiveGame
+from .models import init_db
 from .queries import (
-    init_db,
-    get_user,
     create_user,
+    get_user,
     update_balance,
-    claim_bonus,
-    save_game_result,
-    get_user_stats,
-    get_recent_games,
-    get_leaderboard
+    add_game,
+    get_user_stats
 )
 
 __all__ = [
-    'User',
-    'GameHistory',
-    'ActiveGame',
     'init_db',
-    'get_user',
     'create_user',
+    'get_user',
     'update_balance',
-    'claim_bonus',
-    'save_game_result',
-    'get_user_stats',
-    'get_recent_games',
-    'get_leaderboard'
+    'add_game',
+    'get_user_stats'
 ]
